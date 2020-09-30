c8e7cd222afd3209c61526ce5efbcf98274f49ff
NightKirie <qwer55113322@gmail.com>
2020-09-30 19:47:43 +0800

    Finish PC, doing Register

--- HW1/P76094575/src/PC.sv
+++ HW1/P76094575/src/PC.sv
@@ -1,5 +1,17 @@
 module PC (
-    input clk
+    input clk,
+    input rst
+    input [31:0] pc_in
+    output reg [31:0] pc_out
 );
+
+always @(posedge clk, posedge rst) begin
+    if (rst) begin
+        pc_out <= 32'd0;
+    end
+    else begin
+        pc_out <= pc_in;
+    end
+end
 
 endmodule
---
+++ HW1/P76094575/src/Regsiter.sv
@@ -0,0 +1,11 @@
+module Register (
+    input [4:0] rr1_addr,
+    input [4:0] rr2_addr,
+    input [4:0] wr_addr,
+    input [31:0] wd,
+    input reg_w,
+    output reg [31:0] rr1_data,
+    output reg [31:0] rr2_data
+);
+    
+endmodule
